a1f2ed5fe96e2ed1dd1db0e7495c8e5e255ac48a
Conor <cbrewsterart@gmail.com>
2026-09-11 15:47:14 -0400

    feat(editor): export templates and motion presets

--- src/editor-design-library/index.ts
+++ src/editor-design-library/index.ts
@@ -2,3 +2,5 @@ export * from './core/schema';
 export * from './core/tokens';
 export * from './core/registry';
 export * from './primitives/backgrounds';
+export * from './templates/text';
+export * from './motion/presets';
