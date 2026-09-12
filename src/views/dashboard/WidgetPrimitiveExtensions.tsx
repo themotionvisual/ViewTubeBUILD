4f7c7f3291b4ae66d64ff5482ecfe43542a6af1c
Conor <cbrewsterart@gmail.com>
2026-09-12 16:41:55 -0400

    fix(primitives): remove runtime-dependent CSS import order

--- src/views/dashboard/WidgetPrimitiveExtensions.tsx
+++ src/views/dashboard/WidgetPrimitiveExtensions.tsx
@@ -3,11 +3,6 @@ import { Check, ChevronDown, FileVideo2, Search } from "lucide-react"
 import { WIDGET_BADGE_SPECTRUM, WidgetSelect, WidgetSplitButton, type WidgetBadgeSpectrumName, type WidgetBadgeTone, type WidgetSelectOption } from "./WidgetPrimitives"
 import { VT_SPECTRUM_PALETTE_06 } from "../../styles/toolboxPalette"
 import { widgetSizedControlClasses, type WidgetPrimitiveSize, type WidgetPrimitiveTone as PrimitiveTone } from "./widgetPrimitiveSystem"
-import "./widgetPrimitiveSystem.css"
-import "./widgetPrimitiveVariants.css"
-import "./widgetPrimitiveExactHeights.css"
-import "./widgetPrimitiveTones.css"
-import "./widgetMatrixPrimitives.css"
 
 export type WidgetControlHeight = WidgetPrimitiveSize
 export type WidgetPrimitiveTone = PrimitiveTone
