04298c56524f0291389178a88622910b4ffe53c2
Conor <cbrewsterart@gmail.com>
2026-09-19 01:48:37 -0400

    feat(video-director): expose recipe contracts

--- src/features/video-director/index.ts
+++ src/features/video-director/index.ts
@@ -4,4 +4,4 @@ export * from "./projectSchema"
 export * from "./projectStore"
 export * from "./history"
 export * from "./generationContracts"
-export * from "./conflictEngine"
+export * from "./conflictEngine"\nexport * from "./recipeStore"\n
